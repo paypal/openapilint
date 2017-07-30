94714c937ede4388998e19549446de4bb5d7cd15
Jeff Brateman <jeff.brateman@getbraintree.com>
2017-07-30 17:31:51 -0500

    Refactor operation-payload-put for better and consistent validation

--- test/lib/rules/operation-payload-put.js
+++ test/lib/rules/operation-payload-put.js
@@ -1,94 +1,145 @@
 'use strict';
 
+const _ = require('lodash');
 const assert = require('chai').assert;
 const operationPayloadPutRule = require('../../../lib/rules/operation-payload-put');
 
-describe('operation-tags', () => {
+describe('operation-payload-put', () => {
   const options = true;
 
-  it('should not report errors when the put request body parameter matches a get response', (done) => {
-    const schema = {
-      "paths": {
-        "/pets": {
-          "get": {
-            "responses": {
-              "200": {
-                "description": "sample response",
-                "schema": {
-                  "type": "array",
-                  "items": {
-                    "$ref": "#/definitions/pet"
-                  }
-                }
+  // validSchema is used as the base for all tests to make them easier to read.
+  const validSchema = {
+    "paths": {
+      "/pets": {
+        "get": {
+          "responses": {
+            "200": {
+              "schema": {
+                "$ref": "#/definitions/pet"
               }
             }
-          },
-          "put": {
-            "parameters": [
-              {
-                "name": "limit",
-                "description": "Sample param description",
-                "in": "body",
-                "schema": {
-                  "type": "array",
-                  "items": {
-                    "$ref": "#/definitions/pet"
-                  }
-                }
-              }
-            ]
           }
+        },
+        "put": {
+          "parameters": [
+            {
+              "in": "body",
+              "schema": {
+                "$ref": "#/definitions/pet"
+              }
+            }
+          ]
         }
       }
-    };
+    }
+  };
 
-    const failures = operationPayloadPutRule.validate(options, schema);
+  it('should not report errors when the put request body parameter matches a get response', (done) => {
+    const failures = operationPayloadPutRule.validate(options, validSchema);
 
     assert.equal(failures.size, 0);
     done();
   });
 
-  it('should report error when an operation\'s put request body parameter does not match its get response', (done) => {
-    const schema = {
-      "paths": {
-        "/pets": {
-          "get": {
-            "responses": {
-              "200": {
-                "description": "sample response",
-                "schema": {
-                  "type": "array",
-                  "items": {
-                    "$ref": "#/definitions/pet"
-                  }
-                }
-              }
-            }
-          },
-          "put": {
-            "parameters": [
-              {
-                "name": "limit",
-                "description": "Sample param description",
-                "in": "body",
-                "schema": {
-                  "type": "array",
-                  "items": {
-                    "$ref": "#/definitions/alligator"
-                  }
-                }
-              }
-            ]
-          }
-        }
-      }
-    };
+  it('should report error when valid put request body parameter does not match its valid get response', (done) => {
+    const schema = _.cloneDeep(validSchema);
+    schema["paths"]["/pets"]["put"]["parameters"][0]["schema"]["$ref"] = "#/definitions/alligator";
 
     const failures = operationPayloadPutRule.validate(options, schema);
 
     assert.equal(failures.size, 1);
     assert.equal(failures.get(0).get('location'), 'paths./pets.put.parameters[0].schema');
     assert.equal(failures.get(0).get('hint'), '');
+    done();
+  });
+
+  it('should report error when get 200 response is missing', (done) => {
+    const schema = _.cloneDeep(validSchema);
+    schema["paths"]["/pets"]["get"]["responses"]["200"] = undefined;
+
+    const failures = operationPayloadPutRule.validate(options, schema);
+
+    assert.equal(failures.size, 1);
+    assert.equal(failures.get(0).get('location'), 'paths./pets.get.responses.200');
+    assert.equal(failures.get(0).get('hint'), '');
+    done();
+  });
+
+  it('should report error when get 200 schema is missing', (done) => {
+    const schema = _.cloneDeep(validSchema);
+    schema["paths"]["/pets"]["get"]["responses"]["200"]["schema"] = undefined;
+
+    const failures = operationPayloadPutRule.validate(options, schema);
+
+    assert.equal(failures.size, 1);
+    assert.equal(failures.get(0).get('location'), 'paths./pets.get.responses.200.schema');
+    assert.equal(failures.get(0).get('hint'), '');
+    done();
+  });
+
+  it('should report error when put parameters is missing', (done) => {
+    const schema = _.cloneDeep(validSchema);
+    schema["paths"]["/pets"]["put"]["parameters"] = undefined;
+
+    const failures = operationPayloadPutRule.validate(options, schema);
+
+    assert.equal(failures.size, 1);
+    assert.equal(failures.get(0).get('location'), 'paths./pets.put.parameters');
+    assert.equal(failures.get(0).get('hint'), '');
+    done();
+  });
+
+  it('should report error when put parameters body is missing', (done) => {
+    const schema = _.cloneDeep(validSchema);
+    schema["paths"]["/pets"]["put"]["parameters"] = [];
+
+    const failures = operationPayloadPutRule.validate(options, schema);
+
+    assert.equal(failures.size, 1);
+    assert.equal(failures.get(0).get('location'), 'paths./pets.put.parameters');
+    assert.equal(failures.get(0).get('hint'), '');
+    done();
+  });
+
+  it('should report error when put parameters body schema is missing', (done) => {
+    const schema = _.cloneDeep(validSchema);
+    schema["paths"]["/pets"]["put"]["parameters"][0]["schema"] = undefined;
+
+    const failures = operationPayloadPutRule.validate(options, schema);
+
+    assert.equal(failures.size, 1);
+    assert.equal(failures.get(0).get('location'), 'paths./pets.put.parameters[0].schema');
+    assert.equal(failures.get(0).get('hint'), '');
+    done();
+  });
+
+  it('should report two errors when put and get are missing something', (done) => {
+    const schema = _.cloneDeep(validSchema);
+    schema["paths"]["/pets"]["get"]["responses"]["200"] = undefined;
+    schema["paths"]["/pets"]["put"]["parameters"][0]["schema"] = undefined;
+
+    const failures = operationPayloadPutRule.validate(options, schema);
+
+    assert.equal(failures.size, 2);
+    assert.equal(failures.get(0).get('location'), 'paths./pets.get.responses.200');
+    assert.equal(failures.get(0).get('hint'), '');
+    assert.equal(failures.get(1).get('location'), 'paths./pets.put.parameters[0].schema');
+    assert.equal(failures.get(1).get('hint'), '');
+    done();
+  });
+
+  it('should report two errors when put and get are missing other things', (done) => {
+    const schema = _.cloneDeep(validSchema);
+    schema["paths"]["/pets"]["get"]["responses"]["200"]["schema"] = undefined;
+    schema["paths"]["/pets"]["put"]["parameters"] = [];
+
+    const failures = operationPayloadPutRule.validate(options, schema);
+
+    assert.equal(failures.size, 2);
+    assert.equal(failures.get(0).get('location'), 'paths./pets.get.responses.200.schema');
+    assert.equal(failures.get(0).get('hint'), '');
+    assert.equal(failures.get(1).get('location'), 'paths./pets.put.parameters');
+    assert.equal(failures.get(1).get('hint'), '');
     done();
   });
 });
