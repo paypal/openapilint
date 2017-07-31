4010d7963c1c25f34cbdad36f6b2c8e789780aef
Jeff Brateman <jeff.brateman@getbraintree.com>
2017-07-31 13:31:09 -0500

    Separate common attributes into individual test cases

--- test/lib/all-rules.js
+++ test/lib/all-rules.js
@@ -6,16 +6,16 @@ const List = require('immutable').List;
 
 const rulesPath = 'lib/rules';
 
-describe('all-rules', () => {
-  it('should have common attributes', (done) => {
-    fs.readdir(rulesPath, (err, items) => {
-      assert.isNull(err);
-      assert.isTrue(items.length > 0);
+describe('all-rules should have common attributes', () => {
+  fs.readdir(rulesPath, (err, items) => {
+    assert.isNull(err);
+    assert.isTrue(items.length > 0);
 
-      items.forEach((element) => {
-        const requiredName = path.parse(element).name;
-        const rule = require(`../../${rulesPath}/${requiredName}`);
+    items.forEach((element) => {
+      const requiredName = path.parse(element).name;
+      const rule = require(`../../${rulesPath}/${requiredName}`);
 
+      it(`${requiredName} should have common attributes`, () => {
         // rules should have a non-empty description
         assert.isDefined(rule.description);
         assert.isTrue(rule.description.length > 0);
@@ -23,9 +23,7 @@ describe('all-rules', () => {
         // validating a rule should return a failure list
         const result = rule.validate({}, {});
         assert.isTrue(result instanceof List, `${requiredName} returned a ${result} instead of a List`);
-      });
-
-      done();
+      })
     });
   });
 });
