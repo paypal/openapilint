cedb73328cf9900b47aaac33f041cf9579770a03
Jeff Brateman <jeff.brateman@getbraintree.com>
2017-12-27 15:18:08 -0600

    Fix linting issues

--- test/lib/helpers/PatternOption.js
+++ test/lib/helpers/PatternOption.js
@@ -20,12 +20,14 @@ describe('PatternOption', () => {
   describe('constructor', () => {
     it('Creates a new PatternOption that does not ignore case', () => {
       const option = new PatternOption('testPrefix', { testPrefixPattern: 'testPattern' });
+
       assert.equal('testPattern', option.pattern);
       assert.isFalse(option.ignoreCase);
     });
 
     it('Creates a new PatternOption that ignores case', () => {
       const option = new PatternOption('testPrefix', { testPrefixPatternIgnoreCase: 'testPattern' });
+
       assert.equal('testPattern', option.pattern);
       assert.isTrue(option.ignoreCase);
     });
