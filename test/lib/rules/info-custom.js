42124394c9d8ca48d1760d220b3ce594b16561c9
Jeff Brateman <braebot@gmail.com>
2018-01-05 18:13:11 -0600

    Deprecate docs-path, in favor of a info-custom rule config (#44)

--- test/lib/rules/info-custom.js
+++ test/lib/rules/info-custom.js
@@ -37,4 +37,66 @@ describe('info-custom', () => {
       assert.equal(failures.get(0).get('hint'), 'Expected info description to be present and to match "[a-zA-Z]"');
     });
   });
+
+
+  describe('x-publicDocsPath should be present and valid', () => {
+    const options = {
+      whenField: '$key',
+      whenPattern: '.*',
+      thenField: 'x-publicDocsPath',
+      thenPattern: '^[a-zA-Z0-9-_.]+$'
+    };
+
+    it('should not report errors when x-publicDocsPath is present', () => {
+      const schema = {
+        info: {
+          'x-publicDocsPath': 'myApiPath-includes-dashes'
+        }
+      };
+
+      const failures = infoCustomRule.validate(options, schema);
+
+      assert.equal(failures.size, 0);
+    });
+
+    it('should report error when x-publicDocsPath is not present', () => {
+      const schema = {
+        info: {
+        }
+      };
+
+      const failures = infoCustomRule.validate(options, schema);
+
+      assert.equal(failures.size, 1);
+
+      assert.equal(failures.get(0).get('location'), 'info');
+      assert.equal(failures.get(0).get('hint'), 'Expected info x-publicDocsPath to be present and to match "^[a-zA-Z0-9-_.]+$"');
+    });
+
+    it('should report error when x-publicDocsPath is not well formed', () => {
+      const schema = {
+        info: {
+          'x-publicDocsPath': 'my invalid #path'
+        }
+      };
+
+      const failures = infoCustomRule.validate(options, schema);
+
+      assert.equal(failures.size, 1);
+      assert.equal(failures.get(0).get('location'), 'info');
+      assert.equal(failures.get(0).get('hint'), 'Expected info x-publicDocsPath:"my invalid #path" to match "^[a-zA-Z0-9-_.]+$"');
+    });
+
+    it('should not report error when x-publicDocsPath has a period', () => {
+      const schema = {
+        info: {
+          'x-publicDocsPath': 'path.subpath'
+        }
+      };
+
+      const failures = infoCustomRule.validate(options, schema);
+
+      assert.equal(failures.size, 0);
+    });
+  });
 });
